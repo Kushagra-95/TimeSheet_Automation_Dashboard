2c7139acef2e7c31bfcfca31ae42ff4b3793be14
Kushagra-95 <tbnigam@gmail.com>
2026-08-04 16:51:02 +0530

    update api and build command

--- frontend2/src/services/workflowApi.js
+++ frontend2/src/services/workflowApi.js
@@ -1,47 +1,7 @@
-// import axios from "axios";
-
-// const API = axios.create({
-//     baseURL: "http://localhost:8000/api",
-// });
-
-// export const runWorkflow = async () => {
-//     const response = await API.post("/workflow/run");
-//     return response.data;
-// };
-
-// export const downloadFile = async (filename) => {
-//   const response = await API.get(`/files/download/${encodeURIComponent(filename)}`, {
-//     responseType: "blob",
-//   });
-
-//   return response.data;
-// };
-
-// export const downloadFolder = async (folderName) => {
-//   const response = await API.get(`/folders/download/${encodeURIComponent(folderName)}`,{
-//       responseType: "blob",
-//     }
-//   );
-
-//   return response.data;
-// };
-// export const uploadFile = async (file) => {
-//   const formData = new FormData();
-//   formData.append("file", file);
-
-//   const response = await API.post("/upload", formData, {
-//     headers: {
-//       "Content-Type": "multipart/form-data",
-//     },
-//   });
-
-//   return response.data;
-// };
-
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://timesheet-automation-dashboard.onrender.com/api",
 });
 
 export const uploadFile = async (file) => {
